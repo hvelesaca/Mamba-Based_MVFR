30cb8d1494e9bde972ab7ddab65a0cccd165931b
Henry Velesaca Lara <51929556+hvelesaca@users.noreply.github.com>
2025-05-14 11:29:33 -0500

    Update evaluate_multitask.py

--- scripts/evaluate_multitask.py
+++ scripts/evaluate_multitask.py
@@ -11,6 +11,7 @@ import argparse
 
 # Set random seed for reproducibility
 torch.manual_seed(42)
+
 if torch.cuda.is_available():
     torch.cuda.manual_seed_all(42)
     torch.backends.cudnn.deterministic = True
