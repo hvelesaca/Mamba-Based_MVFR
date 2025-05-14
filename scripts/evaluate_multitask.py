fe639cd7a04527e69ff29fce73b2edfcb2f80e60
Henry Velesaca Lara <51929556+hvelesaca@users.noreply.github.com>
2025-05-14 09:43:01 -0500

    Update evaluate_multitask.py

--- scripts/evaluate_multitask.py
+++ scripts/evaluate_multitask.py
@@ -372,7 +372,7 @@ if __name__ == "__main__":
     
     # Load test dataset based on dataset_type
     filter_data = (args.dataset_type == 'filtered')
-    test_dataset = MVFoulDataset("dataset/test_preprocessed", "dataset/test/annotations.json", 
+    test_dataset = MVFoulDataset("/kaggle/input/datasetmvfd/datasetMVFD/test_preprocessed", "/kaggle/input/datasetmvfd/datasetMVFD/test_preprocessed/annotations.json", 
                                  split='test', preload=True, filter_data=filter_data)
     test_loader = DataLoader(test_dataset, batch_size=24, shuffle=False, collate_fn=custom_collate, 
                              num_workers=0, pin_memory=True)
