c92082bcdea2cb4faaa3ca0593bb0ccc20a53061
Henry Velesaca Lara <51929556+hvelesaca@users.noreply.github.com>
2025-05-16 14:11:17 -0500

    Update model_2.py

--- scripts/model_2.py
+++ scripts/model_2.py
@@ -133,7 +133,7 @@ class MultiTaskModelMamba(nn.Module):
     def __init__(self, dropout=0.7, drop_path_rate=0.3):
         super().__init__()
         self.backbone = video_models.mvit_v2_s(weights=video_models.MViT_V2_S_Weights.KINETICS400_V1)
-        self.unfreeze_partial_backbone(layers_to_unfreeze=4)
+        self.unfreeze_partial_backbone(layers_to_unfreeze=10)
 
         in_features = self.backbone.head[1].in_features
         self.backbone.head[1] = nn.Linear(in_features, 512)
