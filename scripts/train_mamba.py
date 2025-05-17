3bfc819f4c4f286724b7d14823197ca99550229d
Henry Velesaca Lara <51929556+hvelesaca@users.noreply.github.com>
2025-05-17 17:54:13 -0500

    Update train_mamba.py

--- scripts/train_mamba.py
+++ scripts/train_mamba.py
@@ -742,7 +742,7 @@ def train_model(
 
     if scheduler_type == "cosineWarm":
         # Scheduler con CosineAnnealingWarmRestarts
-        scheduler = optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=10, T_mult=2, eta_min=1e-6)
+        scheduler = optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=7, T_mult=2, eta_min=1e-6)
     elif scheduler_type == "stepLR":    
         scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.5)
     elif scheduler_type == "cosine":
@@ -1087,5 +1087,5 @@ if __name__ == "__main__":
         use_mixup=False,       # CAMBIO: pon True para usar mixup
         use_cutmix=False,      # CAMBIO: pon True para usar cutmix (no implementado aquí)
         use_extra_aug=False,    # CAMBIO: pon False para solo augmentaciones básicas
-        scheduler_type="stepLR"  # CAMBIO: pon "cosine, cosineWarm, onecycle, stepLR" para CosineAnnealingLR
+        scheduler_type="cosineWarm"  # CAMBIO: pon "cosine, cosineWarm, onecycle, stepLR" para CosineAnnealingLR
     )
