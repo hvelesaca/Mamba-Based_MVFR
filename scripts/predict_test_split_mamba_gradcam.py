05dcf745cd2f7b6abb79eb4c7d6f94714b68d158
Henry Velesaca Lara <51929556+hvelesaca@users.noreply.github.com>
2025-05-22 13:37:25 -0500

    Update predict_test_split_mamba_gradcam.py

--- scripts/predict_test_split_mamba_gradcam.py
+++ scripts/predict_test_split_mamba_gradcam.py
@@ -1,7 +1,7 @@
 import os
 import torch
 from torch.utils.data import Dataset, DataLoader
-from model_2_v3 import MultiTaskModelMamba
+from model_2_v4 import MultiTaskModelMamba
 from tqdm import tqdm
 import json
 import datetime
@@ -777,7 +777,7 @@ if __name__ == "__main__":
         print("Usando", torch.cuda.device_count(), "GPUs")
         model = torch.nn.DataParallel(model)
     model = model.to(device)
-    model_path = "/kaggle/input/mvfr-v3/pytorch/default/1/best_multitask_mamba_model_epoch4_ba32.7700.pth"
+    model_path = "/kaggle/input/mvfr-v4/pytorch/default/1/best_multitask_mamba_model_epoch5_ba31.2105.pth"
     model.load_state_dict(torch.load(model_path, map_location=device, weights_only=True), strict=True)
 
     # Supongamos que 'model' es tu modelo de PyTorch
