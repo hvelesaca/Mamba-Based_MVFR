fc91fe8cfe8dddaf063cfd16a91f181ca0e2c6db
Henry Velesaca Lara <51929556+hvelesaca@users.noreply.github.com>
2025-05-22 11:46:10 -0500

    Update predict_test_split_mamba_gradcam.py

--- scripts/predict_test_split_mamba_gradcam.py
+++ scripts/predict_test_split_mamba_gradcam.py
@@ -576,7 +576,7 @@ def predict_test_split(model, test_loader, device="cuda" if torch.cuda.is_availa
         print(f"Warning: Only {len(all_action_ids)} samples available, visualizing all.")
         num_samples_to_visualize = len(all_action_ids)
 
-    idx_selected = range(220, 230)
+    idx_selected = [0, 1, 2, 3, 4, 5] + range(220, 230)
     
     #indices = np.random.choice(len(all_action_ids), num_samples_to_visualize, replace=False)
 
